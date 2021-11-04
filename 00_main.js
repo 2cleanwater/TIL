3ddf72810767e3f51df311f6c1d901343c61fff4
2cleanwater <32264455+2cleanwater@users.noreply.github.com>
2021-11-04 15:14:23 +0900

    Add files via upload

--- 00_main.js
+++ 00_main.js
@@ -1,1 +1,17 @@
-console.log('|\\_/|\n|q p|   /}\n( 0 )"""\\\n|"^"`    |\n||_/=\\\\__|');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on('line',function(line){
+    const input = line.split(' ');
+    const a = Number(input[0]);
+    const b = Number(input[1]);
+    console.log(a+b);
+    
+    rl.close();
+}).on('close',function(){
+    process.exit();
+});
